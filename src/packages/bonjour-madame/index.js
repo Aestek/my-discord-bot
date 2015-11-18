4be4ea0dc8068355b1f15f3249695591f371ad48
Thibault Gilles <thibault.gilles@adserverpub.com>
2015-11-18 12:06:00 +0100

    Log errors for bonjourmadame

--- src/packages/bonjour-madame/index.js
+++ src/packages/bonjour-madame/index.js
@@ -4,14 +4,14 @@ var request = require('request');
 module.exports = function(options) {
 	return function(bot) {
 		bot
-			.on(bot.triggers.cron, '0 12 * * *')
+			.on(bot.triggers.cron, '10 12 * * *')
 			.sink(options.sink)
 			.do(function(bot, conf) {
 				var that = this;
 
 				request('http://www.bonjourmadame.fr/', function(err, res, body) {
 					if (err)
-						return;
+						return console.log(err);
 
 					var url = $(body).find('.photo.post').first().find('img').first().attr('src');
 					that.sink('Madam of the day : ' + url);
