971ab6d20ce51c0ee9ca523e71e6fea0370e2d89
Thibault Gilles <thibault.gilles@adserverpub.com>
2015-11-19 15:47:02 +0100

    Add bonjour-madame command

--- src/packages/bonjour-madame/index.js
+++ src/packages/bonjour-madame/index.js
@@ -1,21 +1,40 @@
 var $ = require('cheerio');
 var request = require('request');
 
+function getMadam(callback) {
+	var that = this;
+
+	request('http://www.bonjourmadame.fr/', function(err, res, body) {
+		if (err)
+			return console.log(err);
+
+		var url = $(body).find('.photo.post').first().find('img').first().attr('src');
+		callback('Madam of the day : ' + url);
+	});
+}
+
 module.exports = function(options) {
 	return function(bot) {
 		bot
 			.on(bot.triggers.cron, '0 10 * * *')
 			.sink(options.sink)
+			.name('fetch-bonjour-madame')
+			.do(function(bot, conf) {
+				var that = this;
+
+				getMadam(function(msg) {
+					that.sink(msg);
+				});
+			});
+
+		bot
+			.on(bot.triggers['mention-command'], 'bonjour-madame')
 			.name('bonjour-madame')
 			.do(function(bot, conf) {
 				var that = this;
 
-				request('http://www.bonjourmadame.fr/', function(err, res, body) {
-					if (err)
-						return console.log(err);
-
-					var url = $(body).find('.photo.post').first().find('img').first().attr('src');
-					that.sink('Madam of the day : ' + url);
+				getMadam(function(msg) {
+					that.reply(msg);
 				});
 			});
 	}
