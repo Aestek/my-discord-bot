ee5f27cdc326ef5219796b9dd5036bc977d485e8
Thibault Gilles <thibault.gilles@adserverpub.com>
2015-11-24 14:33:51 +0100

    Update raccoon.

--- src/packages/raccoon/index.js
+++ src/packages/raccoon/index.js
@@ -4,20 +4,26 @@ var $ = require('cheerio');
 module.exports = function(bot) {
 	bot
 		.on(bot.triggers.command, 'raccoon')
-		.do(function() {
+		.do(function(bot, conf, args) {
 			var that = this;
-			request(
-				'http://thechive.com/2008/12/06/worlds-biggest-collection-of-cute-raccoons-40-photos/',
-				function(err, res, body) {
-					if (err)
-						return;
 
-					var imgs = [];
-					$(body).find('section[itemprop="articleBody"] img').each(function(i, el) {
-						imgs.push($(el).attr('src'));
-					});
+			this.updatingMessage(args.message, [
+				function(cb) { cb('*Fetching a nice raccoon pic for you...*'); },
+				function(cb) {
+					request(
+						'http://thechive.com/2008/12/06/worlds-biggest-collection-of-cute-raccoons-40-photos/',
+						function(err, res, body) {
+							if (err)
+								return;
 
-					that.reply(imgs[Math.round(Math.random() * (imgs.length - 1))]);
-				});
+							var imgs = [];
+							$(body).find('section[itemprop="articleBody"] img').each(function(i, el) {
+								imgs.push($(el).attr('src'));
+							});
+
+							cb(imgs[Math.round(Math.random() * (imgs.length - 1))]);
+						});
+				}
+			]);
 		});
 };
